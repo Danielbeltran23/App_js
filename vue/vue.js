a038262d24d4a874e01a9d1854153800b80ccceb
unknown <you@example.com>
2023-01-17 11:19:19 -0600

    vue

---
+++ vue/vue.js
@@ -0,0 +1,18 @@
+vue.createapp({
+    data() {
+        return {
+           contador:0
+
+        }
+
+    },
+    methods: {
+        incrementar(){
+            this.contador++;
+        },
+        decrementar(){
+            if[decremtar=0]
+
+        }
+    },
+}).mount('#app');
